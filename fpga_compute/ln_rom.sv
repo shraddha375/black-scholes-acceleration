a0c0b736a8774e850b9d74d48b315658f5f3071c
shraddha375 <39151337+shraddha375@users.noreply.github.com>
2025-12-10 00:05:19 +0530

    Update ln_rom.sv

--- fpga_compute/ln_rom.sv
+++ fpga_compute/ln_rom.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/04/2025 02:29:00 PM
-// Design Name: 
-// Module Name: ln_rom
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module ln_rom(
     input logic [9:0] addr,
