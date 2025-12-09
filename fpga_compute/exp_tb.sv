bc67177b51915758cb82c2910a414c706f983292
shraddha375 <39151337+shraddha375@users.noreply.github.com>
2025-12-10 00:05:00 +0530

    Update exp_tb.sv

--- fpga_compute/exp_tb.sv
+++ fpga_compute/exp_tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/04/2025 05:02:44 PM
-// Design Name: 
-// Module Name: exp_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module exp_tb(
 
