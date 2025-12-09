9f41edaa657241962513182096e95bfe14bebf05
shraddha375 <39151337+shraddha375@users.noreply.github.com>
2025-12-10 00:05:35 +0530

    Update ln_tb.sv

--- fpga_compute/ln_tb.sv
+++ fpga_compute/ln_tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/04/2025 04:11:38 PM
-// Design Name: 
-// Module Name: ln_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module ln_tb(
 
