43d56b8dc18a9bfbf436829df9f0520b38f1839a
shraddha375 <39151337+shraddha375@users.noreply.github.com>
2025-12-10 10:52:43 +0530

    Add SPI pin constraints for SCK, MOSI, MISO, and CS

---
+++ spi/constraints.tcl
@@ -0,0 +1,15 @@
+## SPI SCK - A5
+set_property PACKAGE_PIN G17 [get_ports spi_sck]
+set_property IOSTANDARD LVCMOS33 [get_ports spi_sck]
+
+## SPI MOSI - A6
+set_property PACKAGE_PIN J18 [get_ports spi_mosi]
+set_property IOSTANDARD LVCMOS33 [get_ports spi_mosi]
+
+## SPI MISO - A7
+set_property PACKAGE_PIN K15 [get_ports spi_miso]
+set_property IOSTANDARD LVCMOS33 [get_ports spi_miso]
+
+## SPI CS - A8
+set_property PACKAGE_PIN L16 [get_ports spi_cs]
+set_property IOSTANDARD LVCMOS33 [get_ports spi_cs]
